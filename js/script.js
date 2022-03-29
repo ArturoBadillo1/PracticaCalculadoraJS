da7cd475003f3d328dfacecfdc831828c9911c48
ArturoBadillo <joaba0641@gmail.com>
2022-03-29 13:41:14 -0600

    Cambios 2

--- js/script.js
+++ js/script.js
@@ -1,59 +1,58 @@
 function suma (){
-    var num1 = parseFloat(document.getElementById('numero1').value);
-    var num2 = parseFloat(document.getElementById('numero2').value);
-    var resultado = num1 + num2;
-    if(isNaN(resultado)){
-        document.getElementById('resultado').value = "Error de Operación...";
+    var num1 = parseFloat(document.getElementById('n1').value);
+    var num2 = parseFloat(document.getElementById('n2').value);
+    var r = num1 + num2;
+    if(isNaN(r)){
+        document.getElementById('r').value = "Error de Operación...";
     }else{
-        document.getElementById('resultado').value = resultado;
+        document.getElementById('r').value = r;
     }
 }
 
 function resta(){
-    var num1 = parseFloat(document.getElementById('numero1').value);
-    var num2 = parseFloat(document.getElementById('numero2').value);
-    var resultado = num1 - num2;
-    if(isNaN(resultado)){
-        document.getElementById('resultado').value = "Error de Operación...";
+    var num1 = parseFloat(document.getElementById('n1').value);
+    var num2 = parseFloat(document.getElementById('n2').value);
+    var r = num1 - num2;
+    if(isNaN(r)){
+        document.getElementById('r').value = "Error de Operación...";
     }else{
-        document.getElementById('resultado').value = resultado;
+        document.getElementById('r').value = r;
     }
 }
 
 function division(){
-    var num1 = parseFloat(document.getElementById('numero1').value);
-    var num2 = parseFloat(document.getElementById('numero2').value);
-    var resultado = num1 / num2;
-    if(isNaN(resultado)){
-        document.getElementById('resultado').value = "Error de Operación...";
+    var num1 = parseFloat(document.getElementById('n1').value);
+    var num2 = parseFloat(document.getElementById('n2').value);
+    var r = num1 / num2;
+    if(isNaN(r)){
+        document.getElementById('r').value = "Error de Operación...";
     }else{
-        document.getElementById('resultado').value = resultado;
+        document.getElementById('r').value = r;
     }
 }
 function multiplicacion(){
     var num1 = parseFloat(document.getElementById('numero1').value);
     var num2 = parseFloat(document.getElementById('numero2').value);
-    var resultado = num1 * num2;
-    if(isNaN(resultado)){
-        document.getElementById('resultado').value = "Error de Operación";
+    var r = num1 * num2;
+    if(isNaN(r)){
+        document.getElementById('r').value = "Error de Operación";
     }else{
-        document.getElementById('resultado').value = resultado;
+        document.getElementById('r').value = r;
     }
 }
-
 function potencia(){
     var num1 = parseFloat(document.getElementById('numero1').value);
     var num2 = parseFloat(document.getElementById('numero2').value);
-    var resultado = num1 ** num2;
-    if(isNaN(resultado)){
-        document.getElementById('resultado').value = "Error de Operación...";
+    var r = num1 ** num2;
+    if(isNaN(r)){
+        document.getElementById('r').value = "Error de Operación...";
     }else{
-        document.getElementById('resultado').value = resultado;
+        document.getElementById('r').value = resultado;
     }
 }
 
 function limpiar (){
     document.getElementById('numero1').value = null;
     document.getElementById('numero2').value = null;
-    document.getElementById('resultado').value = null;
+    document.getElementById('r').value = null;
 }
