4bb9d7856ac543823eadbf025e6d8b9d109cc4c5
ArturoBadillo <joaba0641@gmail.com>
2022-03-29 14:03:01 -0600

    update2

--- js/script.js
+++ js/script.js
@@ -2,35 +2,54 @@ function suma (){
     var num1 = parseFloat(document.getElementById('numero1').value);
     var num2 = parseFloat(document.getElementById('numero2').value);
     var resultado = num1 + num2;
-    document.getElementById('resultado').value = resultado;
+    if(isNaN(resultado)){
+        document.getElementById('resultado').value = "Error de Operación...";
+    }else{
+        document.getElementById('resultado').value = resultado;
+    }
 }
 
 function resta(){
     var num1 = parseFloat(document.getElementById('numero1').value);
     var num2 = parseFloat(document.getElementById('numero2').value);
     var resultado = num1 - num2;
-    document.getElementById('resultado').value = resultado;
-    
+    if(isNaN(resultado)){
+        document.getElementById('resultado').value = "Error de Operación...";
+    }else{
+        document.getElementById('resultado').value = resultado;
+    }
 }
 
 function division(){
     var num1 = parseFloat(document.getElementById('numero1').value);
     var num2 = parseFloat(document.getElementById('numero2').value);
     var resultado = num1 / num2;
-    document.getElementById('resultado').value = resultado;
+    if(isNaN(resultado)){
+        document.getElementById('resultado').value = "Error de Operación...";
+    }else{
+        document.getElementById('resultado').value = resultado;
+    }
 }
 function multiplicacion(){
     var num1 = parseFloat(document.getElementById('numero1').value);
     var num2 = parseFloat(document.getElementById('numero2').value);
     var resultado = num1 * num2;
-    document.getElementById('resultado').value = resultado;
+    if(isNaN(resultado)){
+        document.getElementById('resultado').value = "Error de Operación";
+    }else{
+        document.getElementById('resultado').value = resultado;
+    }
 }
 
 function potencia(){
     var num1 = parseFloat(document.getElementById('numero1').value);
     var num2 = parseFloat(document.getElementById('numero2').value);
     var resultado = num1 ** num2;
-    document.getElementById('resultado').value = resultado;
+    if(isNaN(resultado)){
+        document.getElementById('resultado').value = "Error de Operación...";
+    }else{
+        document.getElementById('resultado').value = resultado;
+    }
 }
 
 function limpiar (){
